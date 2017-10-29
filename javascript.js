5eed4bbc033e90ce09777bd765dbf2aa6c321418
nguyenhachuy <nguyenhachuy@gmail.com>
2017-10-28 18:37:59 -0700

    Added working example

--- javascript.js
+++ javascript.js
@@ -1,34 +1,54 @@
 var config = {
-    apiKey: "AIzaSyDmUOQG0sWgN57n-u26a8pJGeyT0Z3YMWc",
-    authDomain: "bootcamp-demo-project.firebaseapp.com",
-    databaseURL: "https://bootcamp-demo-project.firebaseio.com",
-    projectId: "bootcamp-demo-project",
-    storageBucket: "bootcamp-demo-project.appspot.com",
-    messagingSenderId: "297576267697"
+    apiKey: "AIzaSyAyCExyUKAi8ZxurGIdXMOI12fekQA-2AM",
+    authDomain: "musicapp-e8c67.firebaseapp.com",
+    databaseURL: "https://musicapp-e8c67.firebaseio.com",
+    projectId: "musicapp-e8c67",
+    storageBucket: "",
+    messagingSenderId: "317115906434"
 };
+
 firebase.initializeApp(config);
-$(document).ready(function () {
-    var snapshotDiv = $('#snapshot');
-    var dataDiv = $('#data');
-    var firebaseRef = firebase.database().ref('/example-ref');
-    firebaseRef.on('child_added',
-        function (childRefSnapshot) {
-            console.log(childRefSnapshot);
-            console.log(childRefSnapshot.val());
-            
-            var dataObject = childRefSnapshot.val();
-            
-            var dataAtDataKey = dataObject.name;
-            
-            var stringifiedObject = JSON.stringify(dataObject, null, 4);
-            snapshotDiv.html('<pre>' + stringifiedObject + '</pre>');
-            // The dataKey reference came from our object definition.
-            // dataDiv is defined as a variable above.
-            dataDiv.text(dataAtDataKey);
-        }
-    );
-    $('#submit').on('click', function (event) {
-        var newName = $('#name').val().trim();
-        firebaseRef.push({ name: newName });
+var database = firebase.database();
+$('#add-train').on('click', () => {
+
+    event.preventDefault();
+    //Parse the input
+    var name = $('#train-name-input').val().trim();
+    var destination = $('#train-destination-input').val().trim();
+    var time= $('#train-time-input').val().trim();
+    var frequency = $('#train-frequency-input').val().trim();
+
+    //parse moment into time
+    time = moment(time, 'HH:mm').subtract(10, 'years').format('x');
+
+    console.log(time);
+
+    //Then you push the object into firebase
+
+    database.ref('/train').push({
+        name: name,
+        destination: destination,
+        time: time,
+        frequency: frequency
     })
+    $('#train-name-input').val('');
+    $('#train-destination-input').val('');
+    $('#train-time-input').val('');
+    $('#train-frequency-input').val('');
+
+    //put the time into moment js
+});
+
+database.ref('train').on('child_added', (snapshot) => {
+    //get the data
+    var train_object = snapshot.val();
+    var remainder = moment().diff(moment.unix(train_object.time), 'minutes')%train_object.frequency;
+    var minutes = train_object.frequency - remainder;
+    console.log(minutes);
+    var arrival = moment().add('minutes', 'm').format('hh:mm A');
+
+    //add stuff to the table
+    $('tbody').append('<tr><td>' + train_object.name + '</td><td>' + train_object.destination + 
+        '</td><td>' + train_object.frequency + '</td><td>' + train_object.arrival + '</td><td>' +
+        minutes + '</td></tr>'); 
 });
